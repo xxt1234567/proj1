1193773f3f3182dbce6d29c6a68731062245b87a
xxt1234567 <xxt_abcdefg@163.com>
2024-07-29 17:44:18 +0800

    update03

--- contourdrawer.py
+++ contourdrawer.py
@@ -3,55 +3,101 @@ import numpy as np
 import cv2
 import os
 import argparse
+from random import *
 
-def readjs(fjs):
-    with open(fjs,'r',encoding='utf-8') as file:  
-        json_str=file.read()
-    js=json.loads(json_str)
-    info=js['images']
-    rects=js['annotations']
-    for i in rects:
-        for j in range(len(i['segmentation'])):
-            i['segmentation'][j]=int(i['segmentation'][j])
-    return info,rects
+def draw(fin, fout, seg, cat, showimg=False):
+    '''
+    绘制框体
+    
+    输入：
+    fin：str，输入图片路径
+    fout：str，输出图片路径
+    seg：list，每个元素是一个4 * 2的numpy array，记录一个框体四个顶点的坐标
+    cat：list，框体的类别
+    showimg：bool，是否在运行时展示绘制结果
+    
+    输出：无
+    '''
+    '''
+    cnum = max(cat)
+    seed(0)
+    colors = []
+    for i in range(cnum):
+        rgb = (randint(0, 255), randint(0, 255), randint(0, 255))
+        colors.append(rgb)
+    '''
+    colors = [(0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255)]
+    image = cv2.imread(fin)    
+    for i in range(len(seg)):
+        cv2.drawContours(image, [seg[i]], -1, colors[cat[i] - 1], 1)
+        cv2.putText(image, str(cat[i]), (seg[i][0][0], seg[i][0][1]),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, colors[cat[i] - 1], 1)
+    if showimg:
+        cv2.imshow('image', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    cv2.imwrite(fout, image)
 
-def getcontourdata(info,rects):
-    data={}
-    for i in info:
-        data[i['id']]={}
-        data[i['id']]['file_name']=i['file_name']
-        data[i['id']]['segmentation']=[]
-        data[i['id']]['category_id']=[]
-    for i in rects:
-        data[i['image_id']]['segmentation'].append(np.array(i['segmentation']).reshape(4,2))
-        data[i['image_id']]['category_id'].append(i['category_id'])
-    return data
-
-def drawcontour(data,finput,foutput,showimg=False):
-    colors=[(0,0,0),(255,0,0),(0,255,0),(0,0,255)]
-    for i in data:
-        image=cv2.imread(os.path.join(finput,data[i]['file_name']))
-        for j in range(len(data[i]['segmentation'])):
-            cv2.drawContours(image,[data[i]['segmentation'][j]],-1,colors[data[i]['category_id'][j]-1],1)
-            cv2.putText(image,str(data[i]['category_id'][j]),(data[i]['segmentation'][j][0][0],data[i]['segmentation'][j][0][1]),cv2.FONT_HERSHEY_SIMPLEX,0.5,colors[data[i]['category_id'][j]-1],1)
-        if showimg:
-            cv2.imshow('image',image)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-        cv2.imwrite(os.path.join(foutput,data[i]['file_name']),image)
-
-if __name__ == '__main__':
-    parser=argparse.ArgumentParser(description='Expected 0 to 3 arguments')
-    parser.add_argument('-j','--fjs',type=str,default='result.json')
-    parser.add_argument('-i','--finput',type=str,default=os.path.join(os.getcwd(),'image'))
-    parser.add_argument('-o','--foutput',type=str,default=os.path.join(os.getcwd(),'result'))
-    args=parser.parse_args()
-    fjs=args.fjs
-    finput=args.finput
-    foutput=args.foutput
+def drawcontour(fjs, finput, foutput):
+    '''
+    从json文件中读取数据，并将数据传入draw函数进行绘制
+    
+    输入：
+    fjs：str，json文件路径
+    finput：str，输入图片所在文件夹的路径
+    foutput：str，输出图片所在文件夹的路径
+    
+    输出：无
+    '''
+    with open(fjs, 'r', encoding = 'utf-8') as file:  
+        json_str = file.read()
+    js = json.loads(json_str)
+    img = js['images']
+    anno = js['annotations']
+    imgname = {}
+    for i in img:
+        imgname[i['id']] = i['file_name']
+    id0 = None
+    seg = []
+    cat = []
+    for i in anno:
+        if not(i['image_id'] == id0):
+            if not(id0 is None):
+                draw(os.path.join(finput, imgname[id0]), os.path.join(foutput, imgname[id0]), seg, cat)
+            id0 = i['image_id']
+            seg = []
+            cat = []
+        if (len(i['segmentation']) == 4):
+            temp = [i['segmentation'][0], i['segmentation'][1],
+                    i['segmentation'][0], i['segmentation'][3],
+                    i['segmentation'][2], i['segmentation'][3],
+                    i['segmentation'][2], i['segmentation'][1]]
+            i['segmentation'] = temp
+        i['segmentation'] = list(map(int, i['segmentation']))
+        seg.append(np.array(i['segmentation']).reshape(4, 2))
+        cat.append(i['category_id'])
+    if not(id0 is None):
+        draw(os.path.join(finput, imgname[id0]), os.path.join(foutput, imgname[id0]), seg, cat)    
+        
+def run():
+    '''
+    设置命令行参数并运行
+    
+    输入：无
+    
+    输出：无
+    '''
+    parser = argparse.ArgumentParser(description = 'Expected 0 to 3 arguments')
+    parser.add_argument('-j', '--fjs', type=str, default='result.json')
+    parser.add_argument('-i', '--finput', type=str, default=os.path.join(os.getcwd(), 'image'))
+    parser.add_argument('-o', '--foutput', type=str, default=os.path.join(os.getcwd(), 'result'))
+    args = parser.parse_args()
+    fjs = args.fjs
+    finput = args.finput
+    foutput = args.foutput
     if not(os.path.exists(foutput)):
         os.mkdir(foutput)
-        
-    info,rects=readjs(fjs)
-    data=getcontourdata(info,rects)
-    drawcontour(data,finput,foutput)
+    drawcontour(fjs, finput, foutput)
+
+if __name__ == '__main__':
+    run()
